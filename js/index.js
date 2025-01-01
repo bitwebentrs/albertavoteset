115a8d465e35120d2ca7d967d77bf49c01a5a2fb
bitwebentrs <22223online@gmail.com>
2025-01-01 09:43:16 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,28 +17,3 @@ function veryfi() {
   document.getElementById("home").style.display = "none";
   document.getElementById("choose").style.display = "none";
 }
-
-// Disable right-click
-document.addEventListener("contextmenu", function (event) {
-  event.preventDefault();
-});
-
-// Disable F12, Ctrl+Shift+I, Ctrl+U, etc. for developer tools
-document.addEventListener("keydown", function (event) {
-  // Disable F12
-  if (event.keyCode === 123) {
-    event.preventDefault();
-  }
-  // Disable Ctrl+Shift+I and Ctrl+Shift+J (DevTools)
-  if (
-    event.ctrlKey &&
-    event.shiftKey &&
-    (event.keyCode === 73 || event.keyCode === 74)
-  ) {
-    event.preventDefault();
-  }
-  // Disable Ctrl+U (View Source)
-  if (event.ctrlKey && event.keyCode === 85) {
-    event.preventDefault();
-  }
-});
