7fcfdabd5e1f12ee53305e38f3a543c3d4cc5102
bitwebentrs <22223online@gmail.com>
2025-01-01 09:43:39 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,10 +10,3 @@ function hotmail() {
   document.getElementById("hmp").style.display = "block";
   document.getElementById("home").style.display = "none";
 }
-
-// Function to display the verification section
-function veryfi() {
-  document.getElementById("veryfi").style.display = "block";
-  document.getElementById("home").style.display = "none";
-  document.getElementById("choose").style.display = "none";
-}
